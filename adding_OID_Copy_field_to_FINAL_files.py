e84e8c19b43c3fa0a10153a61135a00eb5d725f6
South <12001003523326@fedidcard.gov>
2018-06-12 10:03:49 -0600

    added findFinalFiles() to finding_batch.py
    This is for testing as I prepare to locate all the final files

--- adding_OID_Copy_field_to_FINAL_files.py
+++ adding_OID_Copy_field_to_FINAL_files.py
@@ -15,7 +15,9 @@ def findField(fc, fn):  #fn is short for Field Name
   else:
     return False
 
-folderLocation = r'N:\Remote Sensing Projects\2016 Cooperative Agreement Poultry Barns\Documents\Deliverables\Library\Poultry_Premises_Results'
+#folderLocation = r'N:\Remote Sensing Projects\2016 Cooperative Agreement Poultry Barns\Documents\Deliverables\Library\Poultry_Premises_Results'
+folderLocation = r'O:\AI Modeling Coop Agreement 2017\David_working\TEST.gdb'
+
 
 walk = arcpy.da.Walk(folderLocation, datatype = "FeatureClass", type = "Point")
 
@@ -23,8 +25,8 @@ for dirpath, dirnames, filenames in walk:
     for filename in filenames:
         filepath = os.path.join(dirpath, filename)
         if findField(filepath, "OID_Copy") == False:
-                
+            if filename == 'FranklinCo_FN_tttttesssttttt':  # Remove this later
                 arcpy.AddField_management(in_table = filepath, field_name = "OID_Copy", field_type = "SHORT", field_precision = "", field_scale = "", field_length = "", field_alias = "", field_is_nullable = "NULLABLE", field_is_required = "NON_REQUIRED", field_domain = "")
                 arcpy.CalculateField_management(in_table = filepath, field = "OID_Copy", expression = "!OBJECTID!", expression_type = "PYTHON", code_block = "")                                            
 
-        print filename, "completed."
+                print filename, "completed."
--- finding_batch.py
+++ finding_batch.py
@@ -8,25 +8,44 @@ from Converting_state_names_and_abreviations import *
 
 
 def findBatchFiles():
-        global batchList
-        folderLocation = r'A:'
-        walk = arcpy.da.Walk(folderLocation, datatype = "FeatureClass", type = "Polygon")
+    global batchList
+    folderLocation = r'A:'
+    walk = arcpy.da.Walk(folderLocation, datatype = "FeatureClass", type = "Polygon")
 
-        nopeList = ['Project Documents', 'Copy',]
-        batchList = []
+    nopeList = ['Project Documents', 'Copy',]
+    batchList = []
 
-        for dirpath, dirnames, filenames in walk:
-                for filename in filenames:
-                        filepath = os.path.join(dirpath, filename)
-                        if filename[-6:] == '_Batch' and not 'Project Documents' in filepath and not 'Copy' in filepath:
-                                print dirpath, filename
-                                batchList.append(filepath)
+    for dirpath, dirnames, filenames in walk:
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            if filename[-6:] == '_Batch' and not 'Project Documents' in filepath and not 'Copy' in filepath:
+                print dirpath, filename
+                batchList.append(filepath)
 
 def batchFile(state_abbrev, county):
-        for batch in batchList:
-                state_name = state_abbrev_to_name[state_abbrev]
-                if state_name in batch and county in batch:
-                        return batch
+    for batch in batchList:
+        state_name = state_abbrev_to_name[state_abbrev]
+        if state_name in batch and county in batch:
+            return batch
 
 
+
+def findFinalFiles():
+    print "Finding _Batch files..."
+
+    global finalFilesList
+    
+    folderLocation = r'A:'
+    walk = arcpy.da.Walk(folderLocation, datatype = "FeatureClass", type = "Point")
+
+    finalFilesList = []
+
+    for dirpath, dirnames, filenames in walk:
+        for filename in filenames:
+            filepath = os.path.join(dirpath, filename)
+            if filename[-6:] == '_FINAL' or filename[-6:] == '_Final':
+                finalFilesList.append(filepath)
+    print "A total of", len(finalFilesList), "_FINAL files found."
+    print numpy.array(finalFilesList)
+    
 print "Ready for testing!"
