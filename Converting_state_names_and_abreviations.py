b1187dd8160c6cdf8766f522d5fd49de3e65ee41
VS-DavidSouth <38262961+VS-DavidSouth@users.noreply.github.com>
2019-10-24 16:10:47 -0400

    added arcGIS arcade mini function

--- Converting_state_names_and_abreviations.py
+++ Converting_state_names_and_abreviations.py
@@ -147,3 +147,12 @@ state_abbrev_to_name = {
     'PR': 'Puerto Rico',
     'VI': 'Virgin Islands',}
 
+# Here is the code for using in ArcGIS Arcade, for use in web maps, etc.
+# Copy the following and remove the '#'
+#var OG_dict ='{"Alabama":"AL","Alaska":"AK","Arizona":"AZ","Arkansas":"AR","California":"CA","Colorado":"CO","Connecticut":"CT","Delaware":"DE","Florida":"FL","Georgia":"GA","Hawaii":"HI","Idaho":"ID","Illinois":"IL","Indiana":"IN","Iowa":"IA","Kansas":"KS","Kentucky":"KY","Louisiana":"LA","Maine":"ME","Maryland":"MD","Massachusetts":"MA","Michigan":"MI","Minnesota":"MN","Mississippi":"MS","Missouri":"MO","Montana":"MT","Nebraska":"NE","Nevada":"NV","New Hampshire":"NH","New Jersey":"NJ","New Mexico":"NM","New York":"NY","North Carolina":"NC","North Dakota":"ND","Ohio":"OH","Oklahoma":"OK","Oregon":"OR","Pennsylvania":"PA","Rhode Island":"RI","South Carolina":"SC","South Dakota":"SD","Tennessee":"TN","Texas":"TX","Utah":"UT","Vermont":"VT","Virginia":"VA","Washington":"WA","West Virginia":"WV","Wisconsin":"WI","Wyoming":"WY","District of Columbia":"DC"}';
+#var dict = Dictionary(OG_dict);
+#var result = '';
+#for (var key in dict){
+#    if($feature["STATE_NAME"]==key) result = dict[key]
+#};
+#return result;
